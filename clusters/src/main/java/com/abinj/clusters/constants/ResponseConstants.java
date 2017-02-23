4ca07e18301ce804d2683d76ec59e21cab1620ab
Abinj <abinmj656@gmail.com>
2017-02-24 01:11:23 +0530

    Second verticle added for handle admin related apis

--- clusters/src/main/java/com/abinj/clusters/constants/ResponseConstants.java
+++ clusters/src/main/java/com/abinj/clusters/constants/ResponseConstants.java
@@ -7,4 +7,7 @@ public class ResponseConstants {
     public static final String FAILURE = "failure";
 
     public static final String USER = "user";
+    public static final String USERS = "users";
+
+    public static final String USERS_FETCHED_SUCCESSFULLY = "Users fetched successfully";
 }
--- clusters/src/main/java/com/abinj/clusters/vertcles/UserHandler.java
+++ clusters/src/main/java/com/abinj/clusters/vertcles/UserHandler.java
@@ -1,44 +1,21 @@
 package com.abinj.clusters.vertcles;
 
-import static com.abinj.clusters.constants.ResponseConstants.*;
-import com.abinj.clusters.dao.UserDao;
 import com.abinj.clusters.models.User;
-import com.abinj.clusters.utility.ResponseUtility;
+import com.abinj.clusters.services.UserService;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.json.JsonObject;
-
-import java.util.concurrent.CompletableFuture;
-
-import static com.abinj.clusters.constants.MongoConstants.EMAIL;
 
 public class UserHandler extends AbstractVerticle {
+    private UserService userService;
 
     @Override
     public void start() throws Exception {
 
         vertx.eventBus().consumer("user.signup", message -> {
-            CompletableFuture<Boolean> future1;
-            CompletableFuture<Object> future2;
 
             User user = (User) message.body();
-            String email = user.getEmail();
-            future1 = UserDao.checkUserDuplicate(EMAIL, email);
-            future2 = future1.thenCompose( result -> {
-                CompletableFuture<Object> future = new CompletableFuture<Object>();
-                if (result) {
-                    String responseMessage = "An account already exist for user:" + user.getEmail();
-                    Object response = ResponseUtility.bindRequestResponse(FAILURE, responseMessage);
-                    future.complete(response);
-                } else {
-                    UserDao.createUser(user);
-                    String responseMessage = "A new account created for user:" + user.getEmail();
-                    JsonObject response = ResponseUtility.bindRequestResponse(SUCCESS, responseMessage);
-                    response.put(USER, UserDao.createUser(user));
-                    future.complete(response);
-                }
-                return future;
+            userService.userSignUp(user).thenAccept(res -> {
+                message.reply(res);
             });
-
         });
     }
 }
